3601cfcf8b778427e53597063012d6550a380608
nomadriding <fundora.a@gmail.com>
2018-11-16 19:03:39 -0500

    Add class

---
+++ src/components/IngredientList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Ingredients from './Ingredients';
+
+class IngredientList extends React.Component {
+    render () {
+       return (
+           <p>This is an Ingredient List</p>
+       ) 
+    }
+}
+
+
+export default IngredientList;
---
+++ src/components/RecipeList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Recipe from './Recipes';
+
+
+class RecipeList extends React.Component {
+    render() {
+        return (
+            <p>This is a recipe List</p>
+        )
+    }
+}
+
+export default RecipeList;
