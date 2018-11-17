89c5edc68e7b589b8b8837f6f8cfa0c563a8f2a3
Jean Russo Saus <sausjeanrusso@gmail.com>
2018-11-17 18:00:56 -0500

    new files added Form.js, Recipe.jsm, Recipes.js, udated App.js

---
+++ src/styles/main.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+// $('button').addClass('special');
+// });
+
+$(document).ready(function(){
+  $('li').appen(<li>'It\'s a element'</li>);
+
+$('.special').css('font-weight', '600');
+$('.special').css('color', 'red');
+
+
+$('.not-special').css('font-weight', '600');
+$('.not-special').css('color', 'blue');
+
+});
+//
+// $('#change-theme-blue').click(function() {
+//    $('#change-theme-blue').css('background-color', 'blue');
+// });
+//
+// $('#change-theme-red').click(function() {
+//    $('#change-theme-red').css('background-color', 'red');
+// });
+//
+// $('#import-people').click(function() {
+//    $.get('/users', function(data) {
+//        data.forEach(element => {
+//
+//        })
+//    });
+// });
+//
+
+
+
+
+
+//
+// $('button').addClass('special');
+//
+// $(document).ready(function() {
+// $('button').addClass('special');
+// });
+// $('#change-theme-blue').click(function() {
+//   $('#change-theme-blue').css('background-color','blue');
+// });
+// $('#change-theme-red').click(function() {
+//   $('#change-theme-red').css('background-color','red');
+// });
+// $('#import-people').click(function() {
+//   $.get('/users', function(data){
+//       data.forEach(element => {
+//          console.log(element)
+//       });
+//   });
+// });
+
+
+
+
+
+// $( "#change-theme-red" ).click( () => {
+//   console.log( "You clicked a button!" );
+// });
+//
+// $( "#change-theme-blue" ).click( () => {
+//   console.log( "You clicked a button!" );
+// });
+//
+// $('#content-wrapper').click(function() {
+//    $('#content').css('font-size', '100px');
+//     if(!color) {
+//         $('#content').css('color', 'blueviolet');
+//         color = true;
+//     } else {
+//        $('#content').css('color', 'blue');
+//         color = false;
+//    }
+//
+// });
