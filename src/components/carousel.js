1710fb6afdea59ab7214156de4a03f8d8c04c473
Alessandra De La Pompa <a.delapompag@gmail.com>
2018-11-18 15:45:38 -0500

    added packages

---
+++ src/components/carousel.js
@@ -0,0 +1,76 @@
+import React from 'react';
+class { Carousel } extends React.Component;
+
+//   render() {
+//     return (
+//   <div>
+//     <Carousel>
+//       <Carousel.Item>
+//         <img width={900} height={500} alt="Pasta allo scarpariello" src="https://www.giallozafferano.it/images/ricette/198/19843/foto_hd/hd650x433_wm.jpg" />
+//         <Carousel.Caption>
+//           <h3>Pasta allo scarpariello</h3>
+//           <p>Spaguetti with tomato and cheese, classic and delicious.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+// <Carousel.Item>
+//    <img width={900} height={500} alt="Italian Chicken Alfredo" src="https://images-gmi-pmc.edge-generalmills.com/5b31acd1-946b-4eb1-bb5c-b9141056178f.jpg" />
+//    <Carousel.Caption>
+//      <h3>Italian Chicken Alfredo</h3>
+//      <p>The classic pasta we just cannot seem to get enough of.</p>
+//    </Carousel.Caption>
+//  </Carousel.Item>
+//  <Carousel.Item>
+//    <img width={900} height={500} alt="Creamy Tuscan Sausage Pasta" src="https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,q_auto/v1/hellofresh_s3/image/tuscan-sausage-and-pepper-spaghetti-743e2dd9.jpg" />
+//    <Carousel.Caption>
+//      <h3>Creamy Tuscan Sausage Pasta</h3>
+//      <p>Quick and easy linguine recipe you can never go wrong with.</p>
+//    </Carousel.Caption>
+//  </Carousel.Item>
+//  <Carousel.Item>
+//    <img width={900} height={500} alt="Marguerita Pizza" src="https://assets.marthastewart.com/styles/wmax-1500/d31/pizza-margherita-0606-mla102155/pizza-margherita-0606-mla102155_sq.jpg?itok=3FuCCotM" />
+//    <Carousel.Caption>
+//      <h3>Marguerita Pizza</h3>
+//      <p>Simple, yet delicious.</p>
+//    </Carousel.Caption>
+//  </Carousel.Item>
+//  <Carousel.Item>
+//    <img width={900} height={500} alt="Pizza Arrotolata" src="https://www.giallozafferano.it/images/ricette/36/3601/foto_hd/hd450x300.jpg" />
+//    <Carousel.Caption>
+//      <h3>Pizza Arrotolata</h3>
+//      <p>A fusion between pizza and a wrap</p>
+//    </Carousel.Caption>
+//  </Carousel.Item>
+//  <Carousel.Item>
+//    <img width={900} height={500} alt="Pizza Fritta" src="http://www.ricettapizzanapoletana.it/images/pizza_fritta_napoletana.jpg" />
+//    <Carousel.Caption>
+//      <h3>Pizza Fritta</h3>
+//      <p>It is just like pizza, but fried!</p>
+//    </Carousel.Caption>
+//  </Carousel.Item>
+//  <Carousel.Item>
+//    <img width={900} height={500} alt="Tiramisu" src="https://cocineroaficionado.com/wp-content/uploads/2015/09/tiraminu-s.jpg" />
+//    <Carousel.Caption>
+//      <h3>Tiramisu</h3>
+//    </Carousel.Caption>
+//  </Carousel.Item>
+//  <Carousel.Item>
+//    <img width={900} height={500} alt="Chocolate Amaretti Cake" src="https://teaandtakes.files.wordpress.com/2015/03/almond-chocolate-amaretto-cake.jpg?w=529&h=353" />
+//    <Carousel.Caption>
+//      <h3>Chocolate Amaretti Cake</h3>
+//    </Carousel.Caption>
+//  </Carousel.Item>
+//  <Carousel.Item>
+//    <img width={900} height={500} alt="Lemon Ricotta Cake" src="https://cmt.azureedge.net/media/orig_lemon_ricotta_cake_with_candied_lemons_20170403134855560196absb4.jpg" />
+//    <Carousel.Caption>
+//      <h3>Lemon Ricotta Cake</h3>
+//    </Carousel.Caption>
+//  </Carousel.Item>
+// </Carousel>
+
+//   </div>
+
+//    )}};
+
+
+
+// export default Carousel;
